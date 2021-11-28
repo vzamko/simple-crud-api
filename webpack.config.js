29bf75079fcc218486f0e97a908cb3dfbe186649
Valera Zamko <valera.zamko@jet.dev>
2021-11-28 13:46:54 +0200

    feat: Add and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  target : `node16`,
+  entry : './index.js',
+  mode: 'production',
+  output : {
+    module: true,
+    filename: 'index_bundle.mjs'
+  },
+  experiments : {
+    outputModule: true,
+  }
+}
